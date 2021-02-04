256ad437df43ec6649a1d34d9493badb5e5c24c1
Takanori Hoshi <takanori.hoshi@gmail.com>
2021-02-03 18:24:52 -0800

    Update URL of random.cat

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -4,7 +4,7 @@ import CrossfadeImg from '../../src';
 
 const fetchCat = async (setImageUrl: (imageUrl: string) => void) => {
   try {
-    const response = await fetch('http://aws.random.cat/meow');
+    const response = await fetch('https://aws.random.cat/meow');
     const data = await response.json();
     setImageUrl(data.file);
   } catch (err) {
@@ -41,7 +41,7 @@ const App = () => {
       <CrossfadeImg src={imageUrl} width='640px' height='480px' objectFit='contain' />
       <p>Credits:</p>
       <ul>
-        <li>Cat images from <a href="http://random.cat/">random.cat</a></li>
+        <li>Cat images from <a href="https://aws.random.cat/">random.cat</a></li>
         <li>Dog images from <a href="https://dog.ceo/">DOG CEO</a></li>
         <li>Source code at <a href="https://github.com/takapro/react-crossfade-img">react-crossfade-img</a></li>
       </ul>
